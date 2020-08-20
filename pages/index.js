ef8c932b108cf25db616ce4d31bea736ea40ca19
Hugh <hugh@harken.cc>
2020-08-20 19:26:47 +1000

    Adding some hoverable arrows to hint at scroll navigation

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,41 @@ import data from '../data.json';
 
 export default function Home() {
   try {
-    let scrollPos = 0;
-    window.addEventListener('scroll', function(){
-        if ((document.body.getBoundingClientRect()).top > scrollPos) {
-            // up
-            if (window.scrollY <= 0) {
-                // top
-                window.scrollTo(0,document.body.offsetHeight);
-            }
-        } else {
-            // down
-            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-                // bottom
-                window.scrollTo(0,0);
-            }
+    let prevScrollPos = 0;
+    // The debounce function receives our function as a parameter
+    const debounce = (fn) => {
+      let frame;
+      return (...params) => {
+        if (frame) cancelAnimationFrame(frame);
+        frame = requestAnimationFrame(() => { fn(...params) });
+      }
+    };
+
+    const storeScroll = () => {
+      let scrollPos = (document.body.getBoundingClientRect()).top;
+      document.documentElement.dataset.scroll = scrollPos;
+      if (scrollPos > prevScrollPos) {
+        // up
+        if (window.scrollY <= 0) {
+            // top
+            window.scrollTo(0,document.body.offsetHeight);
         }
-        scrollPos = (document.body.getBoundingClientRect()).top;
-    });
+      } else {
+          // down
+          if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+              // bottom
+              scrollPos = 1; // pretend to come from above
+              window.scrollTo(0,0);
+          }
+      }
+      prevScrollPos = scrollPos;
+    }
+
+    // Listen for new scroll events, here we debounce our `storeScroll` function
+    document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+
+    // Update scroll position for first time
+    storeScroll();
   } catch(err) { }
   const sections = data.items;
   sections.push({...data.items[0], name:`${data.items[0].name}-extra`});
@@ -34,6 +52,12 @@ export default function Home() {
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main>
+        <div className="halfscreen upper">
+          <i className="arrow up" onClick={() => scrollBy({ left:0, top: -window.innerHeight, behavior: 'smooth'})}></i>
+        </div>
+        <div className="halfscreen lower">
+          <i className="arrow down" onClick={() => scrollBy({ left:0, top: window.innerHeight, behavior: 'smooth'})}></i>
+        </div>
         {sections.map(({name, href, icon, backgroundColor}) => <div key={name} className="section" style={{backgroundColor}}>
           <a href={href}>
             <img className="icon" src={icon}></img>
